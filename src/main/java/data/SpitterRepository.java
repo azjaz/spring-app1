e9872d68c0c019f984cfba252624b6e8a4df3de9
Andrey Ziabko <azhub@hotmail.com>
2020-01-28 12:31:28 +0300

    add Spitter entity and repository for them

--- src/main/java/data/SpitterRepository.java
+++ src/main/java/data/SpitterRepository.java
@@ -1,4 +1,11 @@
 package data;
 
-public interface SpitterRepository {
+import entity.Spitter;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface SpitterRepository extends CrudRepository<Spitter, Long> {
+    List<Spitter> findAll();
+    Spitter findByUsername(String username);
 }
--- src/main/java/entity/Spitter.java
+++ src/main/java/entity/Spitter.java
@@ -1,4 +1,56 @@
 package entity;
 
+import java.util.Objects;
+
 public class Spitter {
+    private final Long id;
+    private final String username;
+    private final String password;
+    private final String firstName;
+    private final String lastName;
+
+    public Spitter(String username, String password, String firstName, String lastName) {
+        this.id = null;
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Spitter spitter = (Spitter) o;
+        return Objects.equals(id, spitter.id) &&
+                Objects.equals(username, spitter.username) &&
+                Objects.equals(password, spitter.password) &&
+                Objects.equals(firstName, spitter.firstName) &&
+                Objects.equals(lastName, spitter.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password, firstName, lastName);
+    }
 }
