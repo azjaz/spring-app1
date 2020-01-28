da25c788a2aaddb78ff43a1f83b382c5389095ea
Andrey Ziabko <azhub@hotmail.com>
2020-01-28 14:49:34 +0300

    add test to show profile view

--- src/main/java/web/SpitterController.java
+++ src/main/java/web/SpitterController.java
@@ -1,9 +1,13 @@
 package web;
 
 import data.SpitterRepository;
+import entity.Spitter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/spitter")
@@ -17,6 +21,17 @@ public class SpitterController {
     @GetMapping("/register")
     public String showRegistrationForm() {
         return "registerForm";
+    }
+    @PostMapping("/register")
+    public String processRegistration(Spitter spitter) {
+        spitterRepository.save(spitter);
+        return "redirect:/spitter/" + spitter.getUsername();
+    }
+    @GetMapping("/{username}")
+    public String showUserProfile(@PathVariable String username, Model model) {
+        Spitter spitter = spitterRepository.findByUsername(username);
+        model.addAttribute(spitter.getUsername(), spitter);
+        return "profile";
     }
 
 }
