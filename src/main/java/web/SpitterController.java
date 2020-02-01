cb5c81a1bdd15a7be0bc5e6bc3c587a51a840667
Andrey Ziabko <azhub@hotmail.com>
2020-02-01 11:37:36 +0300

    add redirect logig into SpitterController

--- src/main/java/web/SpitterController.java
+++ src/main/java/web/SpitterController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.Part;
 import javax.validation.Valid;
@@ -29,18 +30,22 @@ public class SpitterController {
     }
     @PostMapping("/register")
     public String processRegistration(@RequestPart("profilePicture") Part profilePicture,
-                                      @Valid Spitter spitter, Errors errors) throws IOException {
+                                      @Valid Spitter spitter, Errors errors, RedirectAttributes model) throws IOException {
         if(errors.hasErrors()) {
             return "registerForm";
         }
         profilePicture.write("/data/spittr" + profilePicture.getSubmittedFileName());
         spitterRepository.save(spitter);
-        return "redirect:/spitter/" + spitter.getUsername();
+        model.addAttribute("username", spitter.getUsername());
+        model.addFlashAttribute("spitter", spitter);
+        return "redirect:/spitter/{username}";
     }
     @GetMapping("/{username}")
     public String showUserProfile(@PathVariable String username, Model model) {
-        Spitter spitter = spitterRepository.findByUsername(username);
-        model.addAttribute(spitter.getUsername(), spitter);
+        if (!model.containsAttribute("spittr")) {
+            Spitter spitter = spitterRepository.findByUsername(username);
+            model.addAttribute(spitter.getUsername(), spitter);
+        }
         return "profile";
     }
 
