36e5ee4d5c557cf44e127cc13eb43cf260dd7d93
Andrey Ziabko <azhub@hotmail.com>
2020-02-03 16:36:09 +0300

    add users security config of 3 types

---
+++ src/main/java/config/SecurityConfig.java
@@ -0,0 +1,64 @@
+package config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
+
+//    user configuration with in-memory db
+//    @Override
+//    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth
+//                .inMemoryAuthentication()
+//                .withUser("user").password("pass").authorities("ROLE_USER")
+//                .and()
+//                .withUser("admin").password("admin").roles("USER", "ADMIN");
+//    }
+//    user configuration with jdbc db
+//    @Override
+//    public void configure(AuthenticationManagerBuilder authJdbc) throws Exception {
+//        authJdbc
+//                .jdbcAuthentication()
+//                .dataSource(dataSource)
+//                .usersByUsernameQuery("selct username, password, true "
+//                + "from Spittr where username=?")
+//                .authoritiesByUsernameQuery("select username, 'ROLE_USER' from Spitter where username=?")
+//        .passwordEncoder(new BCryptPasswordEncoder(Integer.parseInt("a12")));
+//    }
+//    user configuration with ldap db
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .ldapAuthentication()
+                .userSearchBase("ou=people")
+                .userSearchFilter("(uid={0})")
+                .groupSearchBase("ou=groups")
+                .groupSearchFilter("member={0}")
+                .contextSource()
+                .root("dc=habuma, dc=com")
+                .ldif("classpath:users.ldif");
+    }
+}
